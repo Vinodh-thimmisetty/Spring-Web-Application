a6f3d03e851aaabf4afe8af209c4bc12a2dd82f5
vinodh kumar Thimmisetty <vinodh5052@gmail.com>
2017-09-09 01:24:34 +0530

    HIbernate Validator demo

---
+++ src/main/java/com/vinodh/hibernate/validation/demo/Car.java
@@ -0,0 +1,122 @@
+package com.vinodh.hibernate.validation.demo;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Negative;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ *  // @formatter:off
+ *  @Target({FIELD, METHOD, PARAMETER, TYPE etc}) 
+ * 		There are four types of bean constraints: 
+ * 				1. Field constraints:
+ *						a) Constraints can be applied to fields of any access type (public, private etc.).
+ *   					b) Constraints on static fields are not supported.
+ * 				2. Property constraints (@Getter Method)
+ *						a) The validation engine accesses the state via the property accessor method.
+ * 				3. Container element constraints
+ * 						a) List, Map, Set etc...
+ * 				4. Class constraints
+ *   
+ 
+ *   
+ *  //@formatter:on
+ * 
+ * @author Vinodh Kumar Thimmisetty
+ *
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class Car {
+
+	// Sample Field Level Constraints
+	@NotNull
+	private String manufacturer;
+	@NotNull
+	@Size(min = 2, max = 14)
+	private String licensePlate;
+	@Min(2)
+	private int seatCount;
+
+	// Sample Property Level
+	@Getter(AccessLevel.NONE)
+	private String mfgCountry;
+	@Getter(AccessLevel.NONE)
+	private boolean isGood;
+
+	@NotNull
+	public String getMfgCountry() {
+		return mfgCountry;
+	}
+
+	@AssertTrue
+	public boolean isGood() {
+		return isGood;
+	}
+
+	// Container Level constraints. Can be used for Custom Constraint
+	private Set<@NotNull @Size(min = 2, max = 10) String> emails = Collections.emptySet();
+	private List<@NotNull @Size(min = 2, max = 10) String> description = Collections.emptyList();
+	private Map<@Length(min = 2) String, @Length(max = 5) String> marks = Collections.emptyMap();
+
+	// Built in Bean Validation Constraints
+
+	@DecimalMin(value = "2")
+	@DecimalMax(value = "5", inclusive = false)
+	private BigDecimal bigDecimal;
+
+	@Digits(integer = 5, fraction = 3)
+	private int days;
+
+	@Email
+	@Pattern(regexp = "/^[a-zA-Z0-9]+$/")
+	private String email;
+
+	@Future
+	private Date deathDate;
+
+	@Past
+	private Date birthDate;
+
+	@FutureOrPresent
+	private Date marriageDate;
+
+	@NotBlank // Trailing spaces will be trimmed
+	private String userName;
+
+	@Negative
+	private Integer temperature;
+
+	@Size(min = 2, max = 100)
+	private List<String> friends;
+
+	
+}
---
+++ src/main/java/com/vinodh/hibernate/validation/demo/test/CarTest.java
@@ -0,0 +1,89 @@
+package com.vinodh.hibernate.validation.demo.test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.vinodh.hibernate.validation.demo.Car;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CarTest {
+
+	private static Validator validator;
+
+	@BeforeClass
+	public static void setup() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		validator = factory.getValidator();
+	}
+
+	@Test
+	public void mfgNameNull() {
+		Car c = Car.builder().manufacturer("Vinodh").seatCount(2).licensePlate("12345").isGood(true).build();
+
+		Set<ConstraintViolation<Car>> constraintViolations = validator.validate(c);
+
+		Assert.assertEquals(1, constraintViolations.size());
+		Assert.assertEquals("must not be null", constraintViolations.iterator().next().getMessage());
+
+		c.setMfgCountry("India");
+		constraintViolations = validator.validate(c);
+		Assert.assertEquals(0, constraintViolations.size());
+
+		List<String> descripton = Arrays.asList("", "1", "Vinodh", "Thimmisetty Vinodh");
+		c.setDescription(descripton);
+		constraintViolations = validator.validate(c);
+		// Don't allow any String having Null, length<2 && length >10 in the List
+		Assert.assertEquals(3, constraintViolations.size());
+
+		Set<String> emails = descripton.stream().collect(Collectors.toSet());
+		c.setEmails(emails);
+		constraintViolations = validator.validate(c);
+		// Don't allow any String having Null, length<2 && length >10 in the Set
+		Assert.assertEquals(6, constraintViolations.size());
+
+		Map<String, String> marks = new HashMap<>();
+		marks.put("Telugu", "90");
+		marks.put("Hindi", "90");
+		marks.put("English", "90");
+		marks.put("Maths", "90");
+		marks.put("Science", "90");
+		marks.put("Social", "90");
+		marks.put("a", "1234567890");
+
+		c.setMarks(marks);
+		constraintViolations = validator.validate(c);
+		Assert.assertEquals(8, constraintViolations.size());
+
+		// The Validator interface contains three methods that can be used to either
+		// validate entire entities or just single properties of the entity.
+		c.setManufacturer(null);
+		constraintViolations = validator.validateProperty(c, "manufacturer");
+		constraintViolations = validator.validateValue(Car.class, "manufacturer", "null");
+
+		// ConstraintViolation Methods
+		log.info("Error Message:: {}", constraintViolations.iterator().next().getPropertyPath());
+		Iterator<ConstraintViolation<Car>> iterator = constraintViolations.iterator();
+		while (iterator.hasNext()) {
+			ConstraintViolation<Car> constraintViolation = constraintViolations.iterator().next();
+			//log.info("Error Message:: {}", constraintViolation.getMessageTemplate());
+
+		}
+	}
+
+}
